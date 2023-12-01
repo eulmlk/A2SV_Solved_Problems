6c6b6cb8507c1628ef918022edf98e01fdc6b97c
Eyouel Melkamu <109799626+eulmlk@users.noreply.github.com>
2023-12-01 17:39:19 +0300

    Added solution for Goal Parser Interpretation

---
+++ pythonTrack/goal-parser-interpretation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        i = 0
+        result = ""
+
+        while i < len(command):
+            if command[i] == 'G':
+                result += 'G'
+                i += 1
+            elif command[i + 1] == ')':
+                result += 'o'
+                i += 2
+            else:
+                result += 'al'
+                i += 4
+
+        return result
