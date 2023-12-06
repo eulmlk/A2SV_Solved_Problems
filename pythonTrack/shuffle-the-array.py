c0345e50b3d530ac6173cf3ec61732915fa703ec
Eyouel Melkamu <109799626+eulmlk@users.noreply.github.com>
2023-12-06 17:03:19 +0300

    Added solution for Shuffle the Array

---
+++ pythonTrack/shuffle-the-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        shuffled = []
+
+        for i in range(n):
+            shuffled += nums[i::n]
+            
+        return shuffled
