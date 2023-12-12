f38137d68d6d347dc624de61a297143c5b5ade9f
Eyouel Melkamu <109799626+eulmlk@users.noreply.github.com>
2023-12-12 18:58:56 +0300

    Added solution for Design Authentication Manager

---
+++ pythonTrack/design-authentication-manager.py
@@ -0,0 +1,30 @@
+class AuthenticationManager:
+
+    def __init__(self, timeToLive: int):
+        self.timeToLive = timeToLive
+        self.tokens = {}
+
+    def generate(self, tokenId: str, currentTime: int) -> None:
+        self.tokens[tokenId] = currentTime
+        
+
+    def renew(self, tokenId: str, currentTime: int) -> None:
+        if tokenId in self.tokens and self.tokens[tokenId] + self.timeToLive > currentTime:
+            self.tokens[tokenId] = currentTime
+        
+
+    def countUnexpiredTokens(self, currentTime: int) -> int:
+        count = 0
+
+        for tokenId, time in self.tokens.items():
+            if time + self.timeToLive > currentTime:
+                count += 1
+        
+        return count
+
+
+# Your AuthenticationManager object will be instantiated and called as such:
+# obj = AuthenticationManager(timeToLive)
+# obj.generate(tokenId,currentTime)
+# obj.renew(tokenId,currentTime)
+# param_3 = obj.countUnexpiredTokens(currentTime)
