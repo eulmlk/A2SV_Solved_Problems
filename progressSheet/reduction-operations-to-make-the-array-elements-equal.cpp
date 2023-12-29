eea730c7df40d16f9e574208c2f65531bbf8f91b
Eyouel Melkamu <109799626+eulmlk@users.noreply.github.com>
2023-12-29 18:10:56 +0300

    Added solution for Reduction Operations to Make the Array Elements Equal

--- progressSheet/reduction-operations-to-make-the-array-elements-equal.cpp
+++ progressSheet/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
-        unordered_map<int, int> freqMap;
-
-        for (int num : nums)
-            ++freqMap[num];
+        sort(nums.rbegin(), nums.rend());
+        int n = nums.size();
+        int steps = 0, count = 0;
         
-        vector<pair<int, int>> counts;
-        for (auto [num, freq] : freqMap)
-            counts.emplace_back(num, freq);
-        
-        sort(counts.rbegin(), counts.rend());
+        for (int i = 1; i < n; ++i) {
+            ++count;
 
-        int steps = 0;
-        for (int i = 0; i < counts.size() - 1; ++i) {
-            int freq = counts[i].second;
-            steps += freq;
-            counts[i + 1].second += freq;
+            if (nums[i] != nums[i - 1])
+                steps += count;
         }
 
         return steps;
