04dfa7115b20e6912ddae37adcb621e64c4cb596
Eyouel Melkamu <109799626+eulmlk@users.noreply.github.com>
2024-01-02 19:17:14 +0300

    Added solution for Move Zeroes

--- progressSheet/move-zeroes.py
+++ progressSheet/move-zeroes.py
@@ -3,10 +3,6 @@ class Solution:
         left, right = 0, 0
         n = len(nums)
 
-        while left < n and nums[left] != 0:
-            left += 1
-            right += 1
-
         while left < n and right < n:
             while right < n and nums[right] == 0:
                 right += 1
@@ -15,7 +11,4 @@ class Solution:
                 nums[left], nums[right] = nums[right], nums[left]
             
             left += 1
-    
-    #                     v
-    # 1 2 3 4 1 2 3 2 0 0 0
-    #               ^
+            right += 1
