7dcffe65dd4e642d3019dbca844881bc51c3f79b
Eyouel Melkamu <109799626+eulmlk@users.noreply.github.com>
2024-02-21 10:52:10 +0300

    Add solution for Simplify Path

--- weekBreaks/leetcode/simplify-path.py
+++ weekBreaks/leetcode/simplify-path.py
@@ -1,8 +1,9 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
+        path = path.split('/')
 
-        for token in path.split('/'):
+        for token in path:
             if token == '..':
                 if stack:
                     stack.pop()
