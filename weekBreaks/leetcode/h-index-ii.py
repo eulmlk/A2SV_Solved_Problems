941c776a4c384dc5b45a02c6cfa76a266a7cbd52
Eyouel Melkamu <109799626+eulmlk@users.noreply.github.com>
2024-03-06 14:33:30 +0300

    Add solution for H-Index II

---
+++ weekBreaks/leetcode/h-index-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        def isCited(h):
+            i = bisect_left(citations, h)
+            return n - i >= h
+        
+        left, right = 1, citations[-1]
+        while left <= right:
+            mid = (left + right) // 2
+
+            if isCited(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right
+
